be7bd5db823b5c2367ac1712d1d5975610bfd486
DASchechtman <dschechtman@carthage.edu>
2017-10-09 01:35:00 -0500

    edit of project
    worked on unity project

---
+++ RPG/Assets/_Scirpts/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private Rigidbody player_position;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 600;
+    }
+
+    // Use this for initialization
+    void Start () {
+        player_position = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+        Vector3 new_pos = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        player_position.AddForce(new_pos*Time.deltaTime, ForceMode.Impulse);
+    }
+
+    void updatePositon(float move_x, float move_y)
+    {
+        
+    }
+}
---
+++ RPG/Assets/_Scirpts/Move_with_player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_with_player : MonoBehaviour {
+
+
+    public Transform player_transform;
+    private Transform cam_transform;
+
+	// Use this for initialization
+	void Start () {
+        cam_transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = player_transform.position.x;
+        var y = cam_transform.position.y;
+        var z = player_transform.position.z;
+        Vector3 new_cam_transform = new Vector3(x, y, z);
+        cam_transform.position = new_cam_transform;
+	}
+}
