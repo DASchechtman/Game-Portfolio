f28a1c000ea3859f463c1769cedbaa364cefbf5a
DASchechtman <dschechtman@carthage.edu>
2017-10-14 16:13:36 -0500

    Progress
    I added a feature that locks the screen orientation, and started building the maze for the game

--- RPG/Assets/_Scirpts/Move.cs
+++ RPG/Assets/_Scirpts/Move.cs
@@ -8,17 +8,20 @@ public class Move : MonoBehaviour {
 
     private void Awake()
     {
-        Application.targetFrameRate = 600;
+        Screen.orientation = ScreenOrientation.Landscape;
     }
 
     // Use this for initialization
     void Start () {
         player_position = GetComponent<Rigidbody>();
+        Input.gyro.enabled = false;
 	}
 	
 	// Update is called once per frame
 	private void FixedUpdate () {
-        Vector3 new_pos = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        float x = Input.acceleration.x*10;
+        float y = Input.acceleration.y*10;
+        Vector3 new_pos = new Vector3(x, 0f, y);
         player_position.AddForce(new_pos*Time.deltaTime, ForceMode.Impulse);
     }
 
